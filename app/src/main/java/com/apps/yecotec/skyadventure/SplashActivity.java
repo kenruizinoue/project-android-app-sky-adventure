11997ece99bf927060a9c952576d85e0b8623e06
kenruizinoue <ken.ruiz0721@gmail.com>
2018-02-24 13:16:40 -0600

    Initial commit

---
+++ app/src/main/java/com/apps/yecotec/skyadventure/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.apps.yecotec.skyadventure;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
